03162de79858f989b1184eb9f3614c4df6dd37f3
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-08-19 16:33:43 +0700

    Column Alignment and Arrangement

--- app/src/main/java/com/rujirakongsomran/jc_jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_jetpackcomposelearn/MainActivity.kt
@@ -3,14 +3,12 @@ package com.rujirakongsomran.jc_jetpackcomposelearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +41,11 @@ fun CustomText(text: String) {
 @Composable
 fun DefaultPreview() {
     JC_JetpackComposeLearnTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
             Surface(
                 modifier = Modifier
                     .width(200.dp)
