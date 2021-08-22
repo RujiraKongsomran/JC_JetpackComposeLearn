2ed3cb8194f47e1c983a12619721add4bf247f1b
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-08-22 22:38:33 +0700

    Annotated String

--- app/src/main/java/com/rujirakongsomran/jc_jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_jetpackcomposelearn/MainActivity.kt
@@ -15,9 +15,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -92,23 +96,51 @@ fun RowScope.CustomItem(
     ) {}
 }
 
+@Composable
+fun CustomizationText() {
+    Text(
+        text = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            .background(MaterialTheme.colors.primary)
+            .padding(16.dp)
+            .width(200.dp),
+        color = Color.White,
+        fontSize = MaterialTheme.typography.h6.fontSize,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.End
+    )
+}
+
+@Composable
+fun CustomizationText2() {
+    Text(
+        buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colors.primary,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                ) {
+                    append("A")
+                }
+                append("B")
+                append("C")
+                append("D")
+            }
+        }, modifier = Modifier.width(200.dp)
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewTextCustomization() {
     JC_JetpackComposeLearnTheme {
         Column(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = stringResource(id = R.string.app_name),
-                modifier = Modifier
-                    .background(MaterialTheme.colors.primary)
-                    .padding(16.dp)
-                    .width(200.dp),
-                color = Color.White,
-                fontSize = MaterialTheme.typography.h6.fontSize,
-                fontStyle = FontStyle.Italic,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.End
-            )
+            CustomizationText2()
         }
     }
 }
